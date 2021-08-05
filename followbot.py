5cc2a277e1e23799ca1b2c745ee2e6dff7a89a5e
Cheesez71 <idonsaimbia@gmail.com>
2021-08-05 09:56:15 +0300

    Script file.

--- followbot.py
+++ followbot.py
@@ -1,4 +1,5 @@
 #Created by Cheesez.
+#NOTE: works only for windows!
 import pyautogui
 import time
 import os
@@ -6,12 +7,18 @@ import sys
 import webbrowser
 
 time.sleep(5)
-i = 205509;
+i = 1; #it means the first user of roblox: "ROBLOX".
 while True:
-	webbrowser.open("https://www.roblox.com/users/" + str(i) + "/profile")
-	time.sleep(4)
-	pyautogui.click(1332, 383)
-	time.sleep(1)
-	i = i + 1;
-	os.system("taskkill /f /im chrome.exe")
-	print(str(i))
+	try:
+		webbrowser.open("https://www.roblox.com/users/" + str(i) + "/profile")
+		time.sleep(4)
+		pyautogui.click(1332, 383)
+		time.sleep(1)
+		i = i + 1;
+		os.system("taskkill /f /im chrome.exe")
+		print(str(i))
+	except KeyboardInterrupt:
+		os.system("taskkill /f /im chrome.exe")
+		exit()
+	
+#I hope you enjoyed while you running this code.
